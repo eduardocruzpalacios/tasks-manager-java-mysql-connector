2f0b5d9615c207615cb69da2fe40d7b55f148058
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-25 19:29:41 +0200

    Menu task management

---
+++ src/gui/Menu.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class Menu {
+
+	public static void taskManagement() {
+		System.out.println("TASK MANAGEMENT MENU");
+		System.out.println("1. Create");
+		System.out.println("2. Update");
+		System.out.println("3. Delete");
+		System.out.println("4. Exit");
+	}
+
+}
