f1e5ab6abccc0994a564799aaedd4bd8df392e13
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-25 19:40:49 +0200

    DAO createTask query

---
+++ src/dao/QueryBuilder.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.Task;
+
+public class QueryBuilder {
+
+	public static String createTask(Task task) {
+		return "INSERT INTO tasks (id, title, content, is_completed, date_created) VALUES (NULL, " + task.getTitle()
+				+ ", " + task.getContent() + ", " + task.isCompleted() + ", " + task.getDateCreated() + ")";
+	}
+
+}
