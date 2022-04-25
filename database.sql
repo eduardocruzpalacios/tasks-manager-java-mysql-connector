0e5313d1b33269df2c98f5c44875816215028ee6
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-25 19:00:46 +0200

    Create task query

--- database.sql
+++ database.sql
@@ -10,3 +10,21 @@ CREATE TABLE tasks (
   date_created DATETIME NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE = InnoDB;
+
+/* CRUD QUERIES */
+INSERT INTO
+  tasks (
+    id,
+    title,
+    content,
+    is_completed,
+    date_created
+  )
+VALUES
+  (
+    NULL,
+    'Prueba',
+    'bla bla bla bla',
+    '0',
+    '2022-04-25 18:55:43.000000'
+  );
