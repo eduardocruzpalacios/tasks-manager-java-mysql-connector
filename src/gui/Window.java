52b1233c1a9fdb8fd8860bcf215bbebc319eb154
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-25 19:25:44 +0200

    Window start

---
+++ src/gui/Window.java
@@ -0,0 +1,14 @@
+package gui;
+
+public class Window {
+
+	public static void start() {
+		System.out.println("");
+		System.out.println("****************************************************");
+		System.out.println("*                                                  *");
+		System.out.println("*                 TASKS MANAGEMENT                 *");
+		System.out.println("*                                                  *");
+		System.out.println("****************************************************");
+	}
+
+}
